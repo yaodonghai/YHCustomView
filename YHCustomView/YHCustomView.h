56d65a258e458433c4881e04db1313ed4dca100b
yaodonghai <1032891512@qq.com>
2021-03-30 22:51:03 +0800

    加文件
    加文件

---
+++ YHCustomView/YHCustomView.h
@@ -0,0 +1,16 @@
+//
+//  YHCustomView.h
+//  YHCustomView
+//
+//  Created by 姚东海 on 2021/3/30.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YHCustomView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YHCustomView/YHCustomView.m
@@ -0,0 +1,20 @@
+//
+//  YHCustomView.m
+//  YHCustomView
+//
+//  Created by 姚东海 on 2021/3/30.
+//
+
+#import "YHCustomView.h"
+
+@implementation YHCustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
